a36c7abeb395c3293b28ab3f6d6b4389db1ea549
Kartikeya Mishra <katt1bh4@gmail.com>
2024-06-19 17:29:29 +0530

    create frontend of the library application create button to add books and dialog box for details

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book() {
+  
+}
+
+function addBookToLibrary() {
+  
+}
+
+const open = document.querySelector("[data-open-modal]");
+const modal = document.querySelector("[data-modal]");
+
+open.addEventListener('click',()=>{
+    modal.showModal();
+})
+
+
+
