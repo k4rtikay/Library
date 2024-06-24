1984bb68080cc286ecc0fe8ae396fb4212b537de
Kartikeya Mishra <katt1bh4@gmail.com>
2024-06-24 15:41:35 +0530

    creates the full front end for the book entry modal

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ function addBookToLibrary() {
   
 }
 
-const open = document.querySelector("[data-open-modal]");
-const modal = document.querySelector("[data-modal]");
+const open = document.querySelector(".addBook");
+const modal = document.querySelector(".modal");
 
 open.addEventListener('click',()=>{
     modal.showModal();
 })
 
-
-
+document.addEventListener("click", e => {
+    const dialog = document.querySelector('dialog');
+    if(!dialog.open) return;
+    const dialogDimensions = dialog.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      dialog.close();
+    }
+  })
